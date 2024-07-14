91595dbfaab47b4092bdf3c4d6de5f319a13b162
lucetrez <149235148+lucetrez@users.noreply.github.com>
2024-07-14 18:34:41 +0900

    Update generate_html.sh

--- generate_html.sh
+++ generate_html.sh
@@ -1,50 +1,60 @@
 #!/bin/bash
-golinks_html=build/index.html
+
+glnk_html=build/index.html
+glnk_username="ty"
 
 # Create the build directory if it doesn't exist
 mkdir -p build
 
 # Create the HTML file using the JSON data
-cat <<EOL >$golinks_html
+cat <<EOL >$glnk_html
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Go Links</title>
-    <style>
-      table {
-        border-collapse: collapse;
-        width: 100%;
-      }
-      th,
-      td {
-        border: 1px solid #ddd;
-        padding: 8px;
-        text-align: left;
-      }
-      th {
-        background-color: #f2f2f2;
-      }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Go Link to Your URL - glnk.dev">
+    <meta property="og:title" content="Go Links - $glnk_username.glnk.dev">
+    <meta property="og:description" content="Easily manage your custom short links with glnk.dev.">
+    <meta property="og:image" content="https://raw.githubusercontent.com/glnk-dev/.github/main/favicon.png">
+    <meta property="og:url" content="https://$glnk_username.glnk.dev">
+    <title>Go Links - $glnk_username.glnk.dev</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="icon" href="https://raw.githubusercontent.com/glnk-dev/.github/main/favicon.ico" type="image/x-icon">
 </head>
-<body>
-    <table>
-        <tr>
-            <th>Subpath</th>
-            <th>Redirect Link</th>
-        </tr>
+<body class="bg-gray-100 text-gray-900">
+    <div class="container mx-auto p-4">
+        <header class="mb-8">
+            <h1 class="text-3xl font-bold">Go Links - $glnk_username.glnk.dev</h1>
+            <p class="mt-2 text-lg">Easily manage your custom short links with <a href="https://glnk.dev" class="text-blue-500 hover:underline">glnk.dev</a>.</p>
+        </header>
+        <table class="min-w-full bg-white shadow-md rounded-lg">
+            <thead>
+                <tr>
+                    <th class="py-2 px-4 bg-gray-200 font-semibold text-gray-700">Subpath</th>
+                    <th class="py-2 px-4 bg-gray-200 font-semibold text-gray-700">Redirect Link</th>
+                </tr>
+            </thead>
+            <tbody>
 EOL
 
 # Loop through the JSON data and generate the table rows
-# jq -r '. | to_entries[] | "<tr><td><a href=\"/go" + .key + "\">/go" + .key + "</a></td><td><a href=\"" + .value + "\">" + .value + "</a></td></tr>"' golinks.json >>$golinks_html
-jq -r '. | to_entries[] | "<tr><td><a href=\"" + .key + "\">" + .key + "</a></td><td><a href=\"" + .value + "\">" + .value + "</a></td></tr>"' golinks.json >>$golinks_html
+jq -r '. | to_entries[] | "<tr><td class=\"border-t py-2 px-4\"><a href=\"" + .key + "\" class=\"text-blue-500 hover:underline\">" + .key + "</a></td><td class=\"border-t py-2 px-4\"><a href=\"" + .value + "\" class=\"text-blue-500 hover:underline\">" + .value + "</a></td></tr>"' glnk.json >>$glnk_html
 
 # Complete the HTML file
-echo "</table></body></html>" >>$golinks_html
-echo "Generated $golinks_html successfully!"
+cat <<EOL >>$glnk_html
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+EOL
+
+echo "Generated $glnk_html successfully!"
 
 declare -A redirect_mapping
 
-# Assuming golinks.json contains a valid JSON object with keys and values.
+# Assuming glnk.json contains a valid JSON object with keys and values.
 # For example:
 # {
 #   "subpath1": "redirect_link1",
@@ -55,7 +65,7 @@ declare -A redirect_mapping
 # Read the JSON file and populate the associative array
 while read -r subpath redirect_link; do
     redirect_mapping["$subpath"]=$redirect_link
-done < <(jq -r 'to_entries[] | "\(.key) \(.value)"' golinks.json)
+done < <(jq -r 'to_entries[] | "\(.key) \(.value)"' glnk.json)
 
 # Iterate through the associative array to get the matching order
 for subpath in "${!redirect_mapping[@]}"; do
@@ -64,10 +74,19 @@ for subpath in "${!redirect_mapping[@]}"; do
 
     # Create the content for the index.html file
     content="<!DOCTYPE html>
-<meta charset=utf-8>
-<title>Redirecting to $subpath</title>
-<meta http-equiv=refresh content=\"0; URL=$redirect_link\">
-<link rel=canonical href=\"$redirect_link\">
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Redirecting to $redirect_link</title>
+    <meta http-equiv=\"refresh\" content=\"0; URL=$redirect_link\">
+    <link rel=\"canonical\" href=\"$redirect_link\">
+    <link rel=\"icon\" href=\"https://glnk.dev/favicon.ico\" type=\"image/x-icon\">
+</head>
+<body>
+    <p>If you are not redirected, <a href=\"$redirect_link\">click here</a>.</p>
+</body>
+</html>
 "
 
     # Create the directory for the subpath if it doesn't exist
